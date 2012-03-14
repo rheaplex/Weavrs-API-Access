5a71d8f18b54d71c99b1ba566463c0f8f1ed96ef
Rob Myers <rob@robmyers.org>
2012-03-14 21:43:58 +0000

    Refactor. Really fix unicode output.

---
+++ dump.py
@@ -0,0 +1,59 @@
+# dump.py - Dump a weavr's posts' keywords to file.
+# Copyright (C) 2012  Rob Myers <rob@robmyers.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+################################################################################
+# Imports
+################################################################################
+
+import codecs
+import config
+import gexf
+import sys
+import urllib
+import weavrs
+
+
+################################################################################
+# Tag graph
+################################################################################
+
+def dump_emotions(runs, now):
+    nodes, edges = gexf.emotion_edge_graph(runs)
+    stream = codecs.open("%s-emotion-edges-%s.gexf" %
+                         (urllib.quote(runs[0]['weavr']),
+                          now.strftime('%Y-%m-%d-%H-%M-%S')),
+                         encoding='utf-8', mode='w')
+    gexf.emotion_edge_graph_to_xml(stream, nodes, edges)
+
+def dump_keywords(runs, now):
+    nodes, edges = gexf.keyword_graph(runs)
+    stream = codecs.open("%s-keywords-%s.gexf" %
+                  (urllib.quote(runs[0]['weavr']),
+                   now.strftime('%Y-%m-%d-%H-%M-%S')),
+                         encoding='utf-8', mode='w')
+    gexf.keyword_graph_to_xml(stream, nodes, edges)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "Usage: %s oauth-key oauth-secret" % sys.argv[0]
+        sys.exit(1)
+    access_token = sys.argv[1]
+    access_secret = sys.argv[2]
+    weavr = weavrs.WeavrApiConnection(config, access_token, access_secret)
+    runs, now = weavrs.weavr_runs_all(weavr)
+    dump_emotions(runs, now)
+    dump_keywords(runs, now)
--- gexf.py
+++ gexf.py
@@ -1,4 +1,4 @@
-# keywords.py - Dump a weavr's posts' keywords to file.
+# gexf.py - Transform a weavr's posts' keywords/emotions into a gexf file.
 # Copyright (C) 2012  Rob Myers <rob@robmyers.org>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -19,14 +19,50 @@
 # Imports
 ################################################################################
 
-import config
-import sys
-import urllib
-import weavrs
+
+################################################################################
+# Emotions
+################################################################################
+
+def emotion_edge_graph(runs):
+    """Return a list of node names, and a dict of edge pairs and weights"""
+    node_names = set()
+    edges = dict()
+    for run in runs:
+        emotion = (run['emotion'],)
+        for post in run['posts']:
+            keywords = set(post['keywords'].split())
+            node_names.update(keywords)
+            for keyword in keywords:
+                for other_keyword in keywords:
+                    if other_keyword != keyword:
+                        # Make sure a/b and b/a are counted the same
+                        # (a,b,label)
+                        edge = tuple(sorted([keyword, other_keyword])) + emotion
+                        edges[edge] = edges.get(edge, 0) + 1
+    return list(node_names), edges
+
+def emotion_edge_graph_to_xml(stream, nodes, edges):
+    print >>stream, u'<?xml version="1.0" encoding="UTF-8"?>'
+    print >>stream, u'<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">'
+    print >>stream, u'<graph mode="static" defaultedgetype="undirected">'
+    print >>stream, u'<nodes>'
+    for node in nodes:
+        print >> stream, u'<node id="%s" label = "%s"/>' % (node, node)
+    print >>stream, u'</nodes>'
+    print >>stream, u'<edges>'
+    edge_id = 0
+    for edge, weight in edges.iteritems():
+        print >>stream, u'<edge id="%i" source="%s" target="%s" weight="%f" label="%s" />'\
+            % (edge_id, edge[0], edge[1], weight, edge[2])
+        edge_id += 1
+    print >>stream, u'</edges>'
+    print >>stream, u'</graph>'
+    print >>stream, u'</gexf>'
 
 
 ################################################################################
-# Edge formatting
+# Keywords
 ################################################################################
 
 def keyword_graph(runs):
@@ -62,22 +98,3 @@ def keyword_graph_to_xml(stream, nodes, edges):
     print >>stream, u'</edges>'
     print >>stream, u'</graph>'
     print >>stream, u'</gexf>'
-
-
-################################################################################
-# Tag graph
-################################################################################
-
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print "Usage: %s oauth-key oauth-secret" % sys.argv[0]
-        sys.exit(1)
-    access_token = sys.argv[1]
-    access_secret = sys.argv[2]
-    weavr = weavrs.WeavrApiConnection(config, access_token, access_secret)
-    runs, now = weavrs.weavr_runs_all(weavr)
-    nodes, edges = keyword_graph(runs)
-    stream = open("%s-keywords-%s.gexf" %
-                  (urllib.quote(runs[0]['weavr']),
-                   now.strftime('%Y-%m-%d-%H-%M-%S')), 'w')
-    keyword_graph_to_xml(stream, nodes, edges)
